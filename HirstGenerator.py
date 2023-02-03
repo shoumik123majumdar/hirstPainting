6a390d59f42bc7babcaeedd6734a5a043e611974
Shoumik Majumdar <shoumik123majumdar@gmail.com>
2023-02-03 14:59:09 -0800

    Initial commit

---
+++ HirstGenerator.py
@@ -0,0 +1,59 @@
+from turtle import Turtle
+import colorgram
+import random
+import turtle
+from turtle import Screen
+
+
+turtle.colormode(255)
+class HirstGenerator():
+    def __init__(self,numColors,filePath):
+        self.hirst = Turtle()
+        self.hirst.speed("fast")
+        self.hirst.penup()
+        self.hirst.shape("circle")
+        self.hirst.setpos(-230, -100)
+        self.hirst.setheading(0)
+        self.numColors = numColors
+        self.filePath = filePath
+        self.colors = colorgram.extract(self.filePath, self.numColors)
+
+    def getColors(self):
+        colorTuples = []
+        for color in self.colors:
+            r = color.rgb[0]
+            g = color.rgb[1]
+            b = color.rgb[2]
+            tup = (r, g, b)
+            colorTuples.append(tup)
+        return colorTuples
+
+
+    def getRandColors(self):
+        colorTuples = self.getColors()
+        randColors = []
+        numColors = self.numColors
+        if(self.numColors>len(self.colors)):
+                numColors = len(self.colors)
+        for i in range(100):
+            randomNum = random.randint(0, numColors - 1)
+            randColors.append(colorTuples[randomNum])
+        return randColors
+
+
+
+    def generatePainting(self):
+        colors = self.getRandColors()
+        counter = 0
+        for i in range(10):
+            for z in range(10):
+                color = colors[counter]
+                self.hirst.dot(20,color)
+                self.hirst.forward(40)
+                counter+=1
+            self.hirst.setx(-230)
+            self.hirst.sety(self.hirst.ycor()+40)
+            self.hirst.ht()
+
+
+
---
+++ main.py
@@ -0,0 +1,41 @@
+from HirstGenerator import HirstGenerator
+from turtle import Screen
+from tkinter import*
+from turtle import Turtle
+import colorgram
+import random
+import turtle
+
+
+screen = Screen()
+window = Tk()
+window.title("Hirst Painting Generator")
+window.minsize(width = 300,height=300)
+
+
+filePathLabel = Label(window,text="Enter the image url you want to extract from")
+filePathLabel.pack()
+entry1 = Entry(window)
+entry1.pack()
+
+
+numColorLabel = Label(window,text="Enter the number of colors you want to extract from \n If greater than the total number of colors extracted, it will extract the maximum number of colors")
+numColorLabel.pack()
+entry2 = Entry(window)
+entry2.pack()
+
+def buttonClicked():
+    global filePath
+    filePath = entry1.get()
+    #filePath = 'one-piece.jpg'
+    global numColors
+    numColors = int(entry2.get())
+    hirst = HirstGenerator(numColors,filePath)
+    hirst.generatePainting()
+
+
+
+generateButton = Button(window,text="Click to generate Painting",command = buttonClicked)
+generateButton.pack()
+
+window.mainloop()
